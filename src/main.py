3889794e07fec8d58c8b5cdf31622602e6e0d81e
Jason Conklin <jasonc@beckhoffus.com>
2026-02-11 15:25:17 -0500

    Start adding classes and unit tests

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node);
+
+if __name__ == "__main__":
+    main()
+
